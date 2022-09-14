c8636b2b6755ecacd690f980a7cbbc7aa679b65e
Tomas Birbe <tomas@ROCA-TBIRBE.dmcibic.local>
2022-09-14 10:47:54 -0300

    main: init project

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+export default function App() {
+  return (
+    <>
+      <nav className="">
+        <div>
+          <p>Se parte de nuestro equipo</p>
+          <div>
+            <button>linkedin</button>
+            <button>youtube</button>
+          </div>
+        </div>
+        <ul>
+          <li>Institucional</li>
+          <li>C. de atencion</li>
+          <li>Pacientes</li>
+          <li>Medicos</li>
+          <li>Heritas</li>
+          <li>Lab colegas</li>
+          <li>Mi cibic</li>
+        </ul>
+      </nav>
+      <header />
+      <main>
+        <section>
+          cards
+          <article>pacientes</article>
+          <article>mi cibic</article>
+          <article>heritas</article>
+        </section>
+        <section>
+          carroussel
+          <article />
+          <article />
+          <article />
+        </section>
+      </main>
+      <section>
+        <article />
+      </section>
+      <section>
+        cibic eco data
+        <article />
+        <article />
+        <article />
+      </section>
+      <section>
+        cibic niños
+        <article />
+        <article />
+        <article />
+      </section>
+      <section>experiencia cibic - carroussel</section>
+      <section>
+        acreditaciones y certificaciones
+        <article />
+        <article />
+        <article />
+        <article />
+        <article />
+        <article />
+      </section>
+      <footer>
+        <section>cibic laboratorios</section>
+        <section>ultimas noticias</section>
+        <section>contacto</section>
+        <section>copyright</section>
+      </footer>
+    </>
+  );
+}
